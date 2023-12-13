1aec1f5527ee8b333c7b38e412732b38cc2fc5ca
Duong Quang Vinh <dqvinh20@clc.fitus.edu.vn>
2023-12-13 22:40:07 +0700

    Refactor RootNavigation

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,32 +1,39 @@
-import React, {useCallback, useEffect} from "react";
-import {StyleSheet} from "react-native";
+import React, { useEffect } from "react";
+import { StyleSheet } from "react-native";
 import RootNavigator from "./navigation";
-import RNBootSplash from "react-native-bootsplash";
 
-import {Provider} from "react-redux";
-import {store} from "./redux/store";
-import {GestureHandlerRootView} from "react-native-gesture-handler";
-import socket, {SocketContext} from "./context/socket";
-import StoreInstance from "./constants/StorageInstance";
-import {handleNotificationPermission} from "./utils/Permission";
-import {QueryClientProvider} from "@tanstack/react-query";
-import {queryClient} from "./init";
-import {CalendarProvider} from "react-native-calendars";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import socket, { SocketContext } from "./context/socket";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./init";
+import { CalendarProvider } from "react-native-calendars";
+import Permission from "./utils/PermissionStrategies";
+import { MobxProvider, rootMobxStore } from "./mobx/store";
+import { StatusBar } from "react-native";
+import { Color } from "./constants/Color";
+import NetworkSnackbar from "./components/NetworkSnackbar";
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+});
+
+if (__DEV__) {
+  import("react-query-native-devtools").then(({ addPlugin }) => {
+    addPlugin({ queryClient });
+  });
+}
 
 const App = () => {
-  const onNavigationContainerReady = useCallback(() => {
-    StoreInstance.setStore(store);
-    RNBootSplash.hide();
-  }, []);
-
-  const checkPermission = useCallback(async () => {
-    await handleNotificationPermission();
-  }, []);
-
   useEffect(() => {
-    try {
-      checkPermission();
-    } catch (error) {}
+    const checkPermission = async () => {
+      await Permission.handleNotificationPermission();
+    };
+
+    checkPermission();
   }, []);
 
   return (
@@ -35,9 +42,17 @@ const App = () => {
         <QueryClientProvider client={queryClient}>
           <Provider store={store}>
             <CalendarProvider date="dd-MM-yyyy">
-              <RootNavigator
-                onNavigationContainerReady={onNavigationContainerReady}
-              />
+              <MobxProvider value={rootMobxStore}>
+                <StatusBar
+                  animated={true}
+                  backgroundColor={Color.primary}
+                  showHideTransition={"fade"}
+                  barStyle={"light-content"}
+                  hidden={false}
+                />
+                <RootNavigator />
+                <NetworkSnackbar isSubscribed />
+              </MobxProvider>
             </CalendarProvider>
           </Provider>
         </QueryClientProvider>
@@ -45,11 +60,5 @@ const App = () => {
     </GestureHandlerRootView>
   );
 };
-
-const styles = StyleSheet.create({
-  root: {
-    flex: 1,
-  },
-});
 
 export default App;
--- src/src/navigation/UnAuthorized/index.tsx
+++ src/src/navigation/UnAuthorized/index.tsx
@@ -1,30 +1,27 @@
-import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import React from "react";
-
 import LoginScreen from "~/screens/Login";
-import Test from "~/screens/Test";
-import VerifyCodeScreen from "~/screens/VerifyCodeScreen";
-import {MentorUsRoutes} from "~/types/navigation";
+import { MentorUsRoutes } from "~/types/navigation";
 
 const UnAuthorizedStack =
   createNativeStackNavigator<MentorUsRoutes.UnAuthorized>();
 
 const UnAuthorized = () => {
   return (
-    <UnAuthorizedStack.Navigator screenOptions={{headerShown: false}}>
-      {/* <UnAuthorizedStack.Screen name="testScreen" component={Test} /> */}
+    <UnAuthorizedStack.Navigator screenOptions={{ headerShown: false }}>
       <UnAuthorizedStack.Screen name="loginScreen" component={LoginScreen} />
-      <UnAuthorizedStack.Screen
+
+      {/** WHAT IS THIS USE FOR? RECHECK IT **/}
+      {/* <UnAuthorizedStack.Screen
         name="verifyScreen"
         component={VerifyCodeScreen}
         options={{
           headerShown: true,
           title: "Verify Code Screen",
         }}
-      />
+      /> */}
     </UnAuthorizedStack.Navigator>
   );
 };
 
-///
 export default UnAuthorized;
