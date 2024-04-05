45fd2108a524fa6b25e48f2cd244bb7bb1ad8b60
hieucckha <hieucckha@gmail.com>
2024-04-05 08:34:35 +0700

    Add fallback to render image in IOS

--- src/src/components/BottomSheetModal/Gallery/GalleryItem.tsx
+++ src/src/components/BottomSheetModal/Gallery/GalleryItem.tsx
@@ -1,8 +1,8 @@
-import React, {memo, useCallback, useState} from "react";
+import React, { memo, useState } from "react";
 import equals from "react-fast-compare";
-import {View, Text, TouchableOpacity} from "react-native";
+import { View, Text, TouchableOpacity } from "react-native";
 
-import {StorageMediaAttachemt} from "~/models/media";
+import { StorageMediaAttachemt } from "~/models/media";
 import Helper from "~/utils/Helper";
 import styles from "./styles";
 import FastImage from "react-native-fast-image";
@@ -14,7 +14,7 @@ interface Props {
   single: string | undefined;
 }
 
-const GalleryItem = ({data, onSelect, number, single}: Props) => {
+const GalleryItem = ({ data, onSelect, number, single }: Props) => {
   const [selected, setSelected] = useState<boolean>(false);
 
   const onPress = () => {
@@ -38,7 +38,10 @@ const GalleryItem = ({data, onSelect, number, single}: Props) => {
 
   return (
     <TouchableOpacity onPress={onPress}>
-      <FastImage source={{uri: data.path}} style={styles.mediaContainer}>
+      <FastImage
+        source={{ uri: data.path }}
+        fallback={true}
+        style={styles.mediaContainer}>
         <View style={selected ? styles.selectedIcon : styles.unselectedIcon} />
         {renderPicked()}
         {data.duration ? (
